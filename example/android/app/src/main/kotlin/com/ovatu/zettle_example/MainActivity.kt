021fc30fb959e5a45a334725abbab0ab779396b4
Dave Hole <dave@ovatu.com>
2025-10-17 11:47:43 +1100

    Clean-up

--- example/android/app/src/main/kotlin/com/ovatu/zettle_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/ovatu/zettle_example/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.ovatu.zettle_example
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
